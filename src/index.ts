61c587532500b6d8aa6c16dd42ab6788d448c0e3
Matt Hodges <matthewjhodges94@gmail.com>
2019-06-23 14:10:05 +0100

    Adds getScores endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
 
+import { Score } from './lib/score';
+
 const app = express();
+
+const highScores: Score[] = [];
 
 app.use(express.static(path.join(__dirname, '../')));
 app.set('view engine', 'html');
@@ -10,8 +14,16 @@ app.get('/', (req: Request, res: Response) => {
 	res.render('index');
 });
 
+const getScores = (req: Request, res: Response) => {
+	res.json(highScores);
+};
+
+app.get('/api/getScores', getScores);
+
 const port = 3000;
 app.listen(port, () => {
 	// tslint:disable-next-line: no-console
 	console.log('Server', process.pid, 'listening on port', port);
 });
+
+export default app;
